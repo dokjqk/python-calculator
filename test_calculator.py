e836b8f6e80c63ad466b0f270c5c531285a73339
dokjqk <dokjqk@gmail.com>
2025-11-27 18:40:01 +0800

    fixing code so that github actions works

--- test_calculator.py
+++ test_calculator.py
@@ -5,7 +5,7 @@ from calculator import add, subtract, multiply, divide
 
 def test_add():
     """Test addition function."""
-    assert add(2, 3) != 5
+    assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
